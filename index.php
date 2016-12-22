82e676fb0932ad5bf7b1297128b8713c5ee3aabf
Jacob Randall <Jacob Randall>
2016-12-22 17:02:59 -0600

    Index nav
    nav right

--- index.php
+++ index.php
@@ -16,37 +16,14 @@
     <body>
          
         <nav class="nav">
-             
-            <li><a href="">Home</a></li> 
-            <li><a href="">About</a> 
-              <ul>
-                <li><a href="">The Team</a></li>
-                <li><a href="">History</a></li> 
-                <li><a href="">Vision</a></li> 
-              </ul> 
-            </li> 
-            <li><a href="">Products</a> 
-              <ul> 
-                <li><a href="">Cozy Couch</a></li> 
-                <li><a href="">Great Table</a></li> 
-                <li><a href="">Small Chair</a></li> 
-                <li><a href="">Shiny Shelf</a></li> 
-                <li><a href="">Invisible Nothing</a></li> 
-              </ul> 
-            </li>
-            <li><a href="">Contact</a> 
-              <ul> 
-                <li><a href="">Online</a></li> 
-                <li><a href="">Right Here</a></li> 
-                <li><a href="">Somewhere Else</a></li> 
-              </ul> 
-            </li> 
-        </nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="upload-project.php">Upload Project</a></li>
                 <li><a href="submit-idea.php">Submit an Idea</a></li>
-            </ul>
+            </ul> 
+            
+           
+        </nav>
         </nav>
         <?php
             
