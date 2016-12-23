699990dd72f5aa11ec3670da7a728c996441d8bd
langnerer <Lagnerer22@gmail.com>
2016-12-22 22:54:46 -0600

    three

--- submit-idea.php
+++ submit-idea.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <?php 
+session_start();
     require_once 'init.php';
     $conn = mysqli_connect("localhost", "rileyODS", "riley4ODS!", "bpa2017");
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
     $user = new User();
+    if(Token::check(Verify::get('token'))) {
+        $userID = $_SESSION["id"];
+        $sql = "SELECT `username` FROM `users` WHERE `userID`=$userID;";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                $userName = $row['username'];
+            }
+        }
+        
+    }
 ?>
 <html>
     <head>
@@ -24,7 +36,7 @@
             
         ?>
      
- <form action="action_page.php">
+ <form action="">
   Project name :<br>
   <input type="text" name="project name" value=" ">
   <br>
@@ -35,6 +47,12 @@
 </form>  
   
 </form>
-    </body>
+   <?php 
+    $name = "Project name";
+    $description = "";
+    
+    echo $name, '<br>', $description;
+    ?>
+</body>
     
 </html>
