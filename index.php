0914651daae2193815c6f04cfe5e2f5ee243756d
langnerer <Lagnerer22@gmail.com>
2017-02-04 20:51:41 -0600

    fix?

--- index.php
+++ index.php
@@ -26,10 +26,11 @@ session_start();
                 <li><a href="submit-idea.php">Submit an Idea</a></li>
                 <li><a href="login-register.php">Login/Register</a></li>
                 <li><a href="profile.php">Profile</a></li>
+                <li><a href="View/ideas.php">View/Ideas</a></li>
             </ul>   
         </nav>
-        <div class="row">
-            <div class="col-1">
+        <div class='row'>
+            <div class='col-1'>
                 <script type='text/javascript'>
                 $(document).ready(function() {
                     $(".expander").click(function() {
@@ -37,91 +38,61 @@ session_start();
                     });
                 });
                 </script>
-            </div>
-        </div>
-        <?php 
-            $sql = "SELECT `follow` FROM `follows` WHERE `user`='".$_SESSION['name']."';";
-            $result = mysqli_query($conn, $sql); 
-            $following = array();
-            if(mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_assoc($result)) {
-                    $following[] = $row["follow"];
-                }
-            }
-            $posts = array();
-            foreach($following as $userFollow) {
-                $sql2 = "SELECT * FROM `posts` WHERE `user` = '$userFollow';";
-                $result2 = mysqli_query($conn, $sql2);
-                if(mysqli_num_rows($result2) > 0) {
-                    while($row2 = mysqli_fetch_assoc($result2)) {
-                        $posts[$row2["timestamp"]] = array("title"=>$row2["title"], 
-                                                            "user"=>$row2["user"], 
-                                                            "description"=>$row2["description"], 
-                                                            "steps"=>$row2["steps"],
-                                                            "materials"=>$row2["materials"],
-                                                            "tips"=>$row2["tips"],
-                                                            "time"=>$row2["estTime"],
-                                                            "cost"=>$row2["estCost"],
-                                                            "rating"=>$row2["rated"],
-                                                            "tags"=>$row2["tags"],
-                                                            "postID"=>$row2["postID"]);
-
+                <?php 
+                    $sql = "SELECT `follow` FROM `follows` WHERE `user`='".$_SESSION['name']."';";
+                    $result = mysqli_query($conn, $sql); 
+                    $following = array();
+                    if(mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            $following[] = $row["follow"];
+                        }
                     }
-                }
-            }
-            ksort($posts);
-            foreach(array_reverse($posts) as $elem) { ?>
-                <div class="row" style='width: 80%;'>
-                    <div class="col-3" style="width: 50%;">
+                    $posts = array();
+                    foreach($following as $userFollow) {
+                        $sql2 = "SELECT * FROM `posts` WHERE `user` = '$userFollow';";
+                        $result2 = mysqli_query($conn, $sql2);
+                        if(mysqli_num_rows($result2) > 0) {
+                            while($row2 = mysqli_fetch_assoc($result2)) {
+                                $posts[$row2["timestamp"]] = array("title"=>$row2["title"], 
+                                                                    "user"=>$row2["user"], 
+                                                                    "description"=>$row2["description"], 
+                                                                    "steps"=>$row2["steps"],
+                                                                    "materials"=>$row2["materials"],
+                                                                    "tips"=>$row2["tips"],
+                                                                    "time"=>$row2["estTime"],
+                                                                    "cost"=>$row2["estCost"],
+                                                                    "reviews"=>$row2["ratings"],
+                                                                    "rating"=>$row2["rated"],
+                                                                    "tags"=>$row2["tags"],
+                                                                    "postID"=>$row2["postID"]);
+                                
+                            }
+                        }
+                    }
+                    ksort($posts);
+                    foreach(array_reverse($posts) as $elem) { ?>
                         <div class='project'>
-                                <?php
-                                    include 'library/config.php';
-                                    include 'library/opendb.php';
-
-                                    $query = "SELECT * FROM `upload` WHERE `postID`=".$elem["postID"].";";
-                                    $result = mysqli_query($conn, $query);
-                                    if (mysqli_num_rows($result) > 0) {
-                                        while ($row = mysqli_fetch_assoc($result)) {
-                                            $name = $row["name"];
-                                            $size = $row["size"];
-                                            $type = $row["type"];
-                                            $content = $row["content"];
-                                            
-                                            header("Content-length: $size");
-                                            header("Content-type: $type");
-                                            header("Content-Disposition: attachment; filename=$name");
-                                            echo "img", $content;
-                                            ?>
-                                            
-                                            <?php
-                                        }
-                                    } else {
-                                    }
-                                    include 'library/closedb.php';
-                                ?>
                             <h2><?php echo $elem["title"]; ?></h2>
                             <p><?php echo $elem["description"]; ?></p>
-                            <p><?php echo $elem["time"]; ?></p>
-                            <p><?php echo $elem["cost"]; ?></p>
-                            <a class="expander" href="#">details</a>
+                            
+                            <a class="expander" href="#">click me</a>
                             <div class='hidden' id='details'>
-                                <p><?php echo $elem["steps"]; ?></p>
-                                <p><?php echo $elem["materials"]; ?></p>
-                                <p><?php echo $elem["tips"]; ?></p>
+                                <?php echo $elem["steps"]; ?>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-1" style='width: 20%; float: left;'>
                         <div class='comments'>
                             <?php 
-                                $sqlCom = "SELECT * FROM `comments` WHERE `postID`=".$elem["postID"]." ORDER BY `timestamp` ASC;";
+                                $sqlCom = "SELECT * FROM `comments` WHERE `postID`=".$elem["postID"].";";
                                 $resultCom = mysqli_query($conn, $sqlCom);
-
+                                
                                 $comm = array();
                                 if(mysqli_num_rows($resultCom) > 0) {
                                     while($row3 = mysqli_fetch_assoc($resultCom)) {
-                                        echo "<p>".$row3["user"].": ".$row3["message"]." -posted ".$row3["timestamp"]."</p>";
+                                        $comm[$elem["postID"]] = array("user" => $row3["user"], "message"=> $row3["message"], "timestamp"=> $row3["timestamp"]);
                                     }
+                                }
+                                foreach($comm as $com) {
+                                    echo "<p>".$com["user"].": ".$com["message"]." -posted ".$com["timestamp"]."</p>";
                                 }
                             ?>
                             <form role='form' method='post'>
@@ -138,10 +109,11 @@ session_start();
                                 }
                             ?>
                         </div>
-                    </div>
-                </div>
-            <?php }
-        ?>
+                    <?php }
+                    
+                ?>
+            </div>
+        </div>
     </body>
 </html>
 
